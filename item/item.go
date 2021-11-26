fcd2e7dac8c4849f53fb6058a906c0c0ad83d831
OmarElKhatibCS <omarelkhatib@mail.com>
2021-11-26 16:56:13 +0200

    add an safe update price method

--- item/item.go
+++ item/item.go
@@ -2,6 +2,7 @@ package item
 
 import "sync"
 
+// Item is simplest abstraction of an Item in an auction
 type Item struct {
 	mu            sync.Mutex
 	Name          string
@@ -10,6 +11,7 @@ type Item struct {
 	Currency      string
 }
 
+// New will create a new Item with current price is same as starting price
 func New(name string, startingPrice uint64, currency string) *Item {
 	return &Item{
 		Name:          name,
@@ -17,4 +19,13 @@ func New(name string, startingPrice uint64, currency string) *Item {
 		CurrentPrice:  startingPrice,
 		Currency:      currency,
 	}
+}
+
+// UpdatePrice will update the price of the item
+// even if the new price is lower than old price
+// the caller should decide the logic of controling the price
+func (i *Item) UpdatePrice(newPrice uint64) {
+	i.mu.Lock()
+	defer i.mu.Unlock()
+	i.CurrentPrice = newPrice
 }
