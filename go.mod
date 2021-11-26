e98403e775016d52c0fed6663c26a5bc564d2c76
OmarElKhatibCS <omarelkhatib@mail.com>
2021-11-26 19:19:58 +0200

    Make the struct safer for concurrency
    the Lock method will panic thats what I wanted so making a bool is
    better idea

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/khatibomar/goction
+
+go 1.17
--- item/item.go
+++ item/item.go
@@ -1,38 +1,59 @@
 package item
 
-import "sync"
+import (
+	"errors"
+	"sync"
+)
+
+var (
+	ErrLocked = errors.New("Item is locked")
+	ErrLower  = errors.New("New price is lower than old price")
+)
 
 // Item is simplest abstraction of an Item in an auction
 type Item struct {
 	mu            sync.Mutex
-	Name          string
-	StartingPrice uint64
-	CurrentPrice  uint64
-	Currency      string
+	name          string
+	startingPrice uint64
+	currentPrice  uint64
+	currency      string
+	locked        bool
 }
 
 // New will create a new Item with current price is same as starting price
 func New(name string, startingPrice uint64, currency string) *Item {
 	return &Item{
-		Name:          name,
-		StartingPrice: startingPrice,
-		CurrentPrice:  startingPrice,
-		Currency:      currency,
+		name:          name,
+		startingPrice: startingPrice,
+		currentPrice:  startingPrice,
+		currency:      currency,
 	}
 }
 
 // UpdatePrice will update the price of the item
-// even if the new price is lower than old price
-// the caller should decide the logic of controling the price
-func (i *Item) UpdatePrice(newPrice uint64) {
+// if the new price is lower than old price return an error
+// if the auction is done return an error
+func (i *Item) UpdatePrice(newPrice uint64) error {
 	i.mu.Lock()
 	defer i.mu.Unlock()
-	i.CurrentPrice = newPrice
+	if i.locked {
+		return ErrLocked
+	}
+	if i.currentPrice > newPrice {
+		return ErrLower
+	}
+	i.currentPrice = newPrice
+	return nil
+}
+
+// GetPrice will return the current item price
+// TODO(khatibomar): Should I lock here?
+func (i *Item) GetPrice() uint64 {
+	return i.currentPrice
 }
 
 // Lock will lock the struct
 // The use case of this is when the auction on the Item ends
-// TODO(khatibomar)[Urgent]: is it safe to never Release lock?
 func (i *Item) Lock() {
-	i.mu.Lock()
+	i.locked = true
 }
